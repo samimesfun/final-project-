fd297273f85c0c9143cfe316e2cb678a44475bac
samimesfun <samiestiff79@gmail.com>
2023-12-14 21:26:12 -0500

    car dao and service

--- casestudy/src/main/java/org/perscholas/casestudy/databse/dao/CarDAO.java
+++ casestudy/src/main/java/org/perscholas/casestudy/databse/dao/CarDAO.java
@@ -4,4 +4,6 @@ import org.perscholas.casestudy.databse.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarDAO extends JpaRepository<Car, Long> {
+
+    public Car findById(Integer id);
 }
